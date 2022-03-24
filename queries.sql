924133d5a298b4a085f1c769895d352f8b096bf2
Brian Williams <Bkwilliamscb@gmail.com>
2022-03-24 18:17:50 -0500

    Finished Product

--- queries.sql
+++ queries.sql
@@ -2,6 +2,16 @@
 
 -- Part 2: Test it with SQL
 
+SELECT name FROM employer WHERE location = "St. Louis City";
+
 -- Part 3: Test it with SQL
 
+DROP TABLE job;
+
 -- Part 4: Test it with SQL
+
+SELECT *
+FROM skill LEFT JOIN
+    job_skills on skill.id = job_skills.skill_id
+     WHERE job_skills.job_id IS NOT NULL
+     ORDER BY name ASC;
--- src/main/java/org/launchcode/techjobs/persistent/models/Job.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Job.java
@@ -1,51 +1,55 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-public class Job{
+public class Job extends AbstractEntity {
 
-    @Id
-    @GeneratedValue
-    private int id;
+    @ManyToOne
+    private Employer employer;
 
-    private String name;
+    @ManyToMany
+    private List<Skill> skills;
 
-    private String employer;
-    private String skills;
 
     public Job() {
     }
 
-    public Job(String anEmployer, String someSkills) {
+    public Job( List<Skill> someSkills, Employer employer) {
         super();
-        this.employer = anEmployer;
+        this.employer = employer;
         this.skills = someSkills;
+    }
+
+    public void addSkill(Skill skill) {
+
+        this.skills.add(skill);
     }
 
     // Getters and setters.
 
-    public String getName() {
-        return name;
-    }
+    public Employer getEmployer() {
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmployer() {
         return employer;
     }
 
-    public void setEmployer(String employer) {
+    public void setEmployer(Employer employer) {
+
         this.employer = employer;
     }
 
-    public String getSkills() {
+    public List<Skill> getSkills() {
+
         return skills;
     }
 
-    public void setSkills(String skills) {
+    public void setSkills(List<Skill> skills) {
+
         this.skills = skills;
     }
+
 }
+
